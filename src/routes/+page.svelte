c2f15a946303d197e26818417c8acd20a44bcbbd
Folker Schellenberg <fs@imagine-orange.com>
2023-12-04 15:50:46 +0100

    sveltekit-tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-center text-4xl">Welcome to SvelteKit</h1>
+    <h2 class="text-center text-4xl">with Tailwind.css</h2>
+    <p class="text-center">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</div>
+```
